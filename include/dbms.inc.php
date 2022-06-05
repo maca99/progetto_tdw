0410ea8975244c8ed00bc69ac5e01439a8ac639f
maca99 <matteo.cavasinni@hotmail.it>
2022-06-05 15:29:14 +0200

    struttura cartelle progetto

---
+++ include/dbms.inc.php
@@ -0,0 +1,8 @@
+<?php
+    $mysqli = new mysqli("localhost","root","","db_tdw");
+
+    if(mysqli->connect_errno){
+        printf("Connect falied %s\n", $mysqli->connect_error);
+        exit();
+    }
+?>
