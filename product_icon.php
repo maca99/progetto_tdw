0932b29160359f6b3773be02f342991e29dc59ed
maca99 <matteo.cavasinni@student.univaq.it>
2022-07-01 16:08:49 +0200

    inizio pagina icona-prodotto

---
+++ product_icon.php
@@ -0,0 +1,32 @@
+<?php
+	require "include/dbms.inc.php";
+	require "include/template2.inc.php";
+
+	$main= new Template("dhtml/prodotto_index.html");
+
+	$id= isset($_GET['product_code'])? $_GET['product_code'] : '';
+
+	$oid=$mysqli->query("SELECT * FROM `prodotto` WHERE idprodotto= $id ");
+
+	if(mysqli_num_rows($oid) != 1){
+		echo("prodotto non trovato");
+		exit();
+	}
+
+	$data = $oid->fetch_assoc();
+	foreach($data as $key => $value) {
+		$body->setContent($key, $value);
+	}
+
+	$result=$mysqli->query("SELECT idimmagine FROM `immagine` WHERE  prodotto_idprodotto = $id LIMIT 1");
+	 while($row = mysqli_fetch_array($result)){
+
+        $id_immagine = $row['idimmagine'];
+		$tag="<img src=show.php?id=$id_immagine>";
+        $body->setContent("immagine",$tag);
+    }
+
+
+	$main->setContent("body",$body->get());
+	$main->close();
+?>
