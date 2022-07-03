cbede7afa61e798d87b3ca2428b9aa3412b5c0e7
maca99 <matteo.cavasinni@hotmail.it>
2022-07-03 20:02:20 +0200

    aggiunta prodotti correlati

--- product.php
+++ product.php
@@ -1,16 +1,17 @@
 <?php
 	require "include/dbms.inc.php";
 	require "include/template2.inc.php";
+	include "include/tags/utility.inc.php";
 
 	$main= new Template("dhtml/blank.html");
     $body= new Template("dhtml/product.html");
+	$utility=new utility();
 
 	$id= isset($_GET['product_code'])? $_GET['product_code'] : '';
 
 
 	//informazioni sul prodotto
-	$oid=$mysqli->query("SELECT * FROM `prodotto` WHERE id_prodotto= $id ");
-
+	$oid=$mysqli->query("SELECT * FROM prodotto,categoria WHERE id_prodotto= $id AND prodotto.id_categoria=categoria.id_categoria");
 	if(mysqli_num_rows($oid) != 1){
 		echo("prodotto non trovato");
 		exit();
@@ -19,6 +20,15 @@
 	$data = $oid->fetch_assoc();
 	foreach($data as $key => $value) {
 		$body->setContent($key, $value);
+	}
+
+	//prodotti correlati
+	$categoria=$data['id_categoria'];
+	$prodotto=$data['id_prodotto'];
+	$result=$mysqli->query("SELECT id_prodotto FROM prodotto,categoria WHERE  prodotto.id_categoria=categoria.id_categoria AND categoria.id_categoria=$categoria AND id_prodotto!=$prodotto LIMIT 4");
+
+	while($row = mysqli_fetch_array($result)){
+		$body->setContent("correlati",$utility->product_icon($row['id_prodotto']));
 	}
 
 	//recensioni 
@@ -41,6 +51,8 @@
 		$body->setContent("immagini","<img src=show.php?id=$id_immagine>");
         $body->setContent("immagine","<img src=show.php?id=$id_immagine>");
     }
+
+
 
 
 	$main->setContent("body",$body->get());
