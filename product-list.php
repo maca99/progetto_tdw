f75e4825c344536577e59f7d19d57f9e2fe7456f
maca99 <matteo.cavasinni@hotmail.it>
2022-07-13 18:04:36 +0200

    immagine alla lista

--- product-list.php
+++ product-list.php
@@ -11,14 +11,17 @@
     while($row=mysqli_fetch_array($result)){
         $body->setContent("nome",$row['nome']);
         $body->setContent("categoria",$row['nome_categoria']);
-        $img=$mysqli->query("SELECT idimmagine FROM immagine WHERE prodotto_idprodotto='".$row['id_prodotto']."' LIMIT 1");
-        if(mysqli_num_rows($img)<1){
-            $body->setContent("immagine","<img src='dhtml/img/not_found.png'>");
+
+        //immagine
+        $img=$mysqli->query("SELECT * FROM immagine WHERE prodotto_idprodotto='".$row['id_prodotto']."' LIMIT 1");
+        if(mysqli_num_rows($img)!=1){
+            $body->setContent("immagine","<img whidt='100' height='100' src='dhtml/img/not_found.png'>");
         }else{
             $res=mysqli_fetch_array($img);
-            $tag=$tag=$res['idimmagine'];
-            $body->setContent("immagine","<img src=show.php?id=$tag>");
-        }
+            $tag=$res['idimmagine'];
+            $img="<img whidt='100' height='100' src=show.php?id=$tag>";
+            $body->setContent("immagine",$img);
+            }
 
     }
 
