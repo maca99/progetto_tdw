81f82b672cc455dcc84a4e2f9712d120c5ac8ead
maca99 <matteo.cavasinni@hotmail.it>
2022-07-15 01:23:06 +0200

    rimesso form per inserire immagine ad aggiunta prodotto

--- product-add.php
+++ product-add.php
@@ -5,7 +5,7 @@
     require "include/template2.inc.php";
     require "include/dbms.inc.php";
 
-    $main = new Template("dhtml/blank.html");
+    $main = new Template("dhtml/blank-min.html");
     $body = new Template("dhtml/product-add.html");
 
    
@@ -22,15 +22,44 @@
 
 
         $oid = $mysqli->query("INSERT INTO prodotto  (id_prodotto,nome,prezzo,descrizione_breve,descrizione,dettagli,id_categoria) 
-        VALUES (NULL,'$nome','$prezzo','$descrizione_breve','$descrizione','$dettagli',$categoria)");
-            
-        if (!$oid) {
-            echo $mysqli->error;
-            $main->setContent("message", "10");
+                VALUES (NULL,'$nome','$prezzo','$descrizione_breve','$descrizione','$dettagli',$categoria)");
+
+        $id_prodotto=mysqli_insert_id($mysqli);
+          
+        //verifica se il file è stato caricato
+        $result = is_uploaded_file($_FILES["userImage"]["tmp_name"]);
+
+        //Check if the user has selected an image
+
+        if(!$result){
+            $error="Please select an image to upload";
+        }else{
+
+            //Get the contents of the image
+
+                $size = $_FILES['userImage']['size'];
+                if($size > 16000){
+                    echo"il file è troppo grande!";
+                    exit();
+                }
+                $type= $_FILES['userImage']['type'];
+                $immagine=file_get_contents($_FILES['userImage']['tmp_name']);
+                $immagine=addslashes($immagine);
+                
+                //Insert the image into the database
+                $query = $mysqli->query("INSERT INTO immagine (immagine,prodotto_idprodotto,type) VALUES ('$immagine','$id_prodotto','$type')");
+        }
+        if($query){
+            header("Location: product-list.php");
+        }else{
+            header("Location: product-add.php?error=$error");
+        } 
+        
+    }
+    if(isset($_GET['error'])){
+        $body->setContent("error",$_GET['error']);
     }
 
-
-   
 
     $main->setContent("body", $body->get());
     $main->close();
