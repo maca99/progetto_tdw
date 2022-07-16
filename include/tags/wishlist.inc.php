fcf9f11b0f8d5e824ecdc6ab52670245561801b9
maca99 <matteo.cavasinni@hotmail.it>
2022-07-16 18:38:30 +0200

    fix whishlist

--- include/tags/wishlist.inc.php
+++ include/tags/wishlist.inc.php
@@ -12,13 +12,14 @@
                 if($_SESSION['auth']){
                     $user=$_SESSION['user']['username'];
                     $oid=$mysqli->query("SELECT COUNT(*) as prodotti FROM utente RIGHT JOIN (wishlist,wishlist_has_prodotto,prodotto) 
-                                                ON (utente.id_utente=wishlist.id_cliente 
+                                                ON (utente.username=wishlist.username)
                                                 AND wishlist.id_wishlist=wishlist_has_prodotto.id_wishlist 
                                                 AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) 
                                                 WHERE utente.username='$user'");
-                    
-                    $data=$oid->fetch_assoc();
-                    $number=$data['prodotti'];
+                    if($oid != null){
+                       $data=$oid->fetch_assoc(); 
+                       $number=$data['prodotti'];
+                    }
                 }  
             }
             return $number;
@@ -28,23 +29,16 @@
             global $mysqli;
                 $main=new Template("dhtml\webarch\product-table.html");
                 $totale=0;
-                $result=$mysqli->query("SELECT * FROM  whishlist WHERE utente.id_utente=whishlist.id_utente");
-                if(mysqsli_num_rows($result) > 0){
+                $result=$mysqli->query("SELECT id_wishlist FROM wishlist WHERE wishlist.username='".$_SESSION['user']['username']."'");
+                if(mysqli_num_rows($result) > 0){
 
-                        $oid=$mysqli->query("SELECT nome,prezzo,data FROM whishlist 
-                                                    LEFT JOIN (whishlist_has_prodotto,prodotto) 
-                                                    ON (whishlist.id_whishlist=whishlist_has_prodotto.id_whishlist
-                                                    AND whishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) WHERE prodotto.id_prodotto=$id");
-                        $oid = $oid->fetch_assoc();
-    
+                        $oid=$mysqli->query("SELECT * FROM wishlist LEFT JOIN (wishlist_has_prodotto,prodotto) ON (wishlist.id_wishlist=wishlist_has_prodotto.id_wishlist AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto)");
+                        $data = $oid->fetch_assoc();
                         //dati
                         $main->setContent("nome", $data['nome']);
                         $main->setContent("prezzo", $data['prezzo']);
-                        $main->setContent("quantity",$item['quantity']);
     
-                        $totale=$totale+($item['quantity']*$data['prezzo']);
                     }
-                $main->setContent("totale",$totale);
                 return $main->get();
 
         }
