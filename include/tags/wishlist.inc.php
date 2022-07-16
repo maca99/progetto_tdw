fba2def01a27218fa54894dc4ad0e171d9a8b90c
maca99 <matteo.cavasinni@hotmail.it>
2022-07-16 16:32:09 +0200

    sistemazione immagini index

--- include/tags/wishlist.inc.php
+++ include/tags/wishlist.inc.php
@@ -23,5 +23,30 @@
             }
             return $number;
         }
+
+        function whish_list($name,$data,$pars){
+            global $mysqli;
+                $main=new Template("dhtml\webarch\product-table.html");
+                $totale=0;
+                $result=$mysqli->query("SELECT * FROM  whishlist WHERE utente.id_utente=whishlist.id_utente");
+                if(mysqsli_num_rows($result) > 0){
+
+                        $oid=$mysqli->query("SELECT nome,prezzo,data FROM whishlist 
+                                                    LEFT JOIN (whishlist_has_prodotto,prodotto) 
+                                                    ON (whishlist.id_whishlist=whishlist_has_prodotto.id_whishlist
+                                                    AND whishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) WHERE prodotto.id_prodotto=$id");
+                        $oid = $oid->fetch_assoc();
+    
+                        //dati
+                        $main->setContent("nome", $data['nome']);
+                        $main->setContent("prezzo", $data['prezzo']);
+                        $main->setContent("quantity",$item['quantity']);
+    
+                        $totale=$totale+($item['quantity']*$data['prezzo']);
+                    }
+                $main->setContent("totale",$totale);
+                return $main->get();
+
+        }
     }
 ?>
