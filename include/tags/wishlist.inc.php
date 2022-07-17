a941f0aa836b45a121f7b5e6eff47206a05231a7
maca99 <matteo.cavasinni@hotmail.it>
2022-07-17 17:43:00 +0200

    fix wishlist

--- include/tags/wishlist.inc.php
+++ include/tags/wishlist.inc.php
@@ -10,15 +10,16 @@
             if(isset($_SESSION['auth']) && isset($_SESSION['user'])){
                 
                 if($_SESSION['auth']){
-                    $user=$_SESSION['user']['username'];
-                    $oid=$mysqli->query("SELECT COUNT(*) as prodotti FROM utente RIGHT JOIN (wishlist,wishlist_has_prodotto,prodotto) 
-                                                ON (utente.username=wishlist.username)
+                    $oid=$mysqli->query("SELECT count(*) as prodotti FROM utente  JOIN (wishlist,wishlist_has_prodotto,prodotto) 
+                                                ON (utente.username=wishlist.username 
                                                 AND wishlist.id_wishlist=wishlist_has_prodotto.id_wishlist 
-                                                AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) 
-                                                WHERE utente.username='$user'");
+                                                AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) WHERE utente.username='".$_SESSION['user']['username']."'");
                     if($oid){
                        $data=$oid->fetch_assoc(); 
-                       $number=$data['prodotti'];
+                        $number=$data['prodotti'];
+                    }else{
+                        echo $mysqli->error;
+                        exit;
                     }
                 }  
             }
@@ -27,16 +28,28 @@
 
         function whish_list($name,$data,$pars){
             global $mysqli;
-                $main=new Template("dhtml\webarch\product-table.html");
+                $main=new Template("dhtml\webarch\wish-list.html");
                 $totale=0;
                 $result=$mysqli->query("SELECT id_wishlist FROM wishlist WHERE wishlist.username='".$_SESSION['user']['username']."'");
                 if(mysqli_num_rows($result) > 0){
 
-                        $oid=$mysqli->query("SELECT * FROM wishlist LEFT JOIN (wishlist_has_prodotto,prodotto) ON (wishlist.id_wishlist=wishlist_has_prodotto.id_wishlist AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto)");
-                        $data = $oid->fetch_assoc();
-                        //dati
-                        $main->setContent("nome", $data['nome']);
-                        $main->setContent("prezzo", $data['prezzo']);
+                        $oid=$mysqli->query("SELECT *  FROM utente  JOIN (wishlist,wishlist_has_prodotto,prodotto) 
+                                                        ON (utente.username=wishlist.username 
+                                                        AND wishlist.id_wishlist=wishlist_has_prodotto.id_wishlist 
+                                                        AND wishlist_has_prodotto.id_prodotto=prodotto.id_prodotto) WHERE utente.username='".$_SESSION['user']['username']."'");
+                        if($oid){
+                            //dati
+                            while($data = $oid->fetch_assoc()){
+                                $main->setContent("nome", $data['nome']);
+                                $main->setContent("data", $data['data']);
+                                $main->setContent("prezzo", $data['prezzo']);
+                            }
+                             
+                        }else{
+                            echo $mysqli->error;
+                            exit;
+                        }
+                        
     
                     }
                 return $main->get();
--- update-wishlist.php
+++ update-wishlist.php
@@ -7,30 +7,36 @@
     $product=isset($_REQUEST['product'])? $_REQUEST['product']:"";
     $action=$_REQUEST['action'];
 
-    $oid=$mysqli->query("SELECT * FROM prodotto WHERE id_prodotto=$product");
-    if(mysqli_num_rows($oid)!=1){
+    if(isset($_SESSION['auth']) && $_SESSION['auth']){
+         $oid=$mysqli->query("SELECT * FROM prodotto WHERE id_prodotto=$product");
+        if(mysqli_num_rows($oid)!=1){
 
-    } else{
+        } else{
 
-    switch($action){
-        case "add":
+        switch($action){
+            case "add":
 
-            $wid=$mysqli->query("SELECT id_wishlist FROM wishlist WHERE wishlist.username='".$_SESSION['auth']['username']."'");
-            while($row=mysqli_fetch_array($wid)){
-                $new_wish=$row['id_wishlist'];
-            }
+                $wid=$mysqli->query("SELECT * FROM wishlist WHERE username='".$_SESSION['user']['username']."'");
+                if(!$wid){
+                    echo $_SESSION['auth']['username'];
+                    echo $mysqli->error;
+                    exit;
+                }
+                $row=mysqli_fetch_array($wid);
+                $id_wish=$row['id_wishlist'];
+                $result= $mysqli->query("INSERT INTO wishlist_has_prodotto (id_wishlist,id_prodotto) VALUES ($id_wish,$product) ");
+                break;
+            case "remove":
+                if(isset($_POST['id_prodotto'])){
 
-            $result= $mysqli->query("INSERT INTO wishlist_has_prodotto (id_wishlist,id_prodotto) VALUES ($new_wish,$product) ");
-            break;
-        case "remove":
-            if(isset($_POST['id_prodotto'])){
-
-                $id = mysqli_real_escape_string($mysqli,$_POST['id_prodotto']);
-            
-                $del = $mysqli -> query("DELETE FROM wishlist_has_prodotto WHERE id_prodotto = '".$id."'") or die(mysqli_error($mysqli));
-            }
-            break;
+                    $id = mysqli_real_escape_string($mysqli,$_POST['id_prodotto']);
+                
+                    $del = $mysqli -> query("DELETE FROM wishlist_has_prodotto WHERE id_prodotto = '".$id."'") or die(mysqli_error($mysqli));
+                }
+                break;
+        }
     }
+   
 }
     header("Location: product.php?product_code=$product");
 
