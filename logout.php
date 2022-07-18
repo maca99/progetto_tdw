6b7407066e96af86b709e0d826e57d31db4c5211
maca99 <matteo.cavasinni@hotmail.it>
2022-07-18 18:31:58 +0200

    fix bug logout

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
 <?php
-session_start();
-if(session_destroy())
-{
-header("Location: login.php");
-}
+
+    session_start();
+
+    require "include/template2.inc.php";
+    require "include/dbms.inc.php";
+
+    unset($_SESSION['auth']);
+    unset($_SESSION['user']);
+    unset($_SESSION['cart']);
+
+    Header("location: index.php");
 ?>
