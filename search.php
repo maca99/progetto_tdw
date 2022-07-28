f13d8070742238d4afdfebea59c8967b94cd5d07
maca99 <matteo.cavasinni@hotmail.it>
2022-07-28 23:58:35 +0200

    fix interfacce ricerca

--- search.php
+++ search.php
@@ -2,18 +2,28 @@
     require "include/dbms.inc.php";
     require "include/template2.inc.php";
 
-    $body= new Template("dhtml/search.html");
+    
     $main= new Template("dhtml/blank-min.html");
 
     $search = mysqli_real_escape_string($mysqli,$_REQUEST['search']);
 
-    $result = $mysqli->query("SELECT * FROM prodotto LEFT JOIN (categoria) ON( prodotto.id_categoria=categoria.id_categoria) WHERE nome LIKE '%".$search."%'");
-
-    while ($row = mysqli_fetch_array($result)){ 
+    $result = $mysqli->query("SELECT * FROM prodotto LEFT JOIN (categoria) ON( prodotto.id_categoria=categoria.id_categoria) WHERE nome LIKE '".$search."%'");
+    $indice=1;button
+    if(mysqli_num_rows($result)==0){
+        $body = new Template("dhtml/fallied-search.html");
+        $body->setContent("ricerca",$search);
+    }else{
+        $body= new Template("dhtml/search.html");
+        while ($row = mysqli_fetch_array($result)){ 
         $body->setContent("id_prodotto",$row['id_prodotto']);
+        $body->setContent("id_categoria",$row['id_categoria']);
         $body->setContent("nome",$row['nome']);
         $body->setContent("categoria",$row['nome_categoria']);
+        $body->setContent("prezzo",$row['prezzo']);
+        $body->setContent("indice",$indice);
+        $indice++;
 
+        
         $img=$mysqli->query("SELECT * FROM immagine WHERE prodotto_idprodotto='".$row['id_prodotto']."' LIMIT 1");
         if(mysqli_num_rows($img)!=1){
             $body->setContent("immagine","<img width='100' height='100' src='dhtml/img/not_found.png'>");
@@ -24,6 +34,10 @@
             $body->setContent("immagine",$img);
             }
         } 
+    }
+        
+
+    
 
         $main->setContent("body",$body->get());
         $main->close();
